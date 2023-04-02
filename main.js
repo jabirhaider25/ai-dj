0b5fa0724fb1e6c1fde88175ba629495865a5ab7
jabirhaider25 <91767507+jabirhaider25@users.noreply.github.com>
2023-04-02 15:03:59 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+var song=""
+
+function setup() {
+    canvas = createCanvas(600,500);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.hide();
+}
+function draw() {
+image(0,0,600,500)
+}
+function preload() {
+song="music.mp3"
+}
+
